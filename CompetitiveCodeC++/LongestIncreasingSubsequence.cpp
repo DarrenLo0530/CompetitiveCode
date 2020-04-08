0e8bd354ac8bf52da82b778e821f622a9c7da5ed
ADADADADZERO <49696301+ADADADADZERO@users.noreply.github.com>
2020-04-07 20:33:27 -0400

    doing bst stuff i dont need to do bruhh

---
+++ CompetitiveCodeC++/LongestIncreasingSubsequence.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define mx 1000000
+
+using namespace std;
+
+int nums[mx+1];
+
+int main(){
+    int N;
+    scanf("%d", &N);
+
+    for(int i = 0; i<N; i++){
+        scanf("%d", &nums[i]);
+    }
+}
